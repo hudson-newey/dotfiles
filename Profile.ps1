8e6040314089409103b0c7b0d4b0dffc8e81fce3
Hudson Newey <neweyh@qut.edu.au>
2023-10-20 14:05:32 +1000

    Create Profile.ps1

---
+++ Profile.ps1
@@ -0,0 +1,25 @@
+function runExit { exit }
+
+function upOne { cd .. }
+function upTwo { cd ..\.. }
+function upThree { cd ..\..\.. }
+function upFour { cd ..\..\..\... }
+
+function lsShort { ls }
+function lsLong { ls }
+
+Set-Alias q runExit
+Set-Alias g git
+Set-Alias vsc code
+
+Set-Alias l lsShort
+
+Set-Alias e lsShort
+Set-Alias ee lsLong
+Set-Alias u cd
+Set-Alias uu upOne
+Set-Alias uuu upTwo
+Set-Alias uuuu upThree
+Set-Alias uuuuu upFour
+
+git lfs install
